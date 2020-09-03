13ddb265287f5c90b699f14b9da0f2c90dca220f
Solbjort Johannesdottir <solbjortjohannesdottir@MacBook-Pro.lan>
2020-09-03 11:24:16 +0000

    first commit

--- max_int.py
+++ max_int.py
@@ -1,6 +1,14 @@
 import math
 num_int = int(input("Input a number: "))    
 # Fill in the missing code
+num1_int = 0
+num2_int = 0
 
-max_int = 100
-print("The maximum is", max_int) 
+while num_int > 0:
+    num1_int = num_int
+    if num1_int > num2_int:
+        num2_int = num1_int
+    else:
+        num_int = int(input("Input a number: ")) 
+
+print("The maximum is", num2_) 
